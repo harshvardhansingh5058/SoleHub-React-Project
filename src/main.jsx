82c9a504c22d3e9ea5c942e5cc9d0ddbe2010b9a
Harshvardhan Singh <harshvardhan01081998@gmail.com>
2026-06-16 02:15:19 +0530

    Fix GitHub Pages base path

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import './assets/css/style.css'
 import Home from './pages/Home.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <Home />
-  </BrowserRouter>,
+  </HashRouter>,
 )
